939bba6113a82b37936a3350479f06993d6e9330
Jessi Shank <jessishank@level11.com>
2018-04-19 13:54:20 -0700

    bumping version

--- ocelot.rb
+++ ocelot.rb
@@ -3,7 +3,7 @@ class Ocelot < Formula
   homepage "https://bitbucket.org/level11consulting/ocelot"
   url "https://s3-us-west-2.amazonaws.com/ocelotty/mac-ocelot-0.2.1-3-gccd406c-dev.zip"
   sha256 "8eecae759f04bd6a97ab2fd99807a6c53e6c6d0b22e23b8319d289fdf562d5b8"
-  version "0.2.1"
+  version "0.2.2"
 
   bottle :unneeded
 
