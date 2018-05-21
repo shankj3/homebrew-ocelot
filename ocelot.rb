008fe7d30c2e00e767b6e6194017b75a09f07fe5
Jessi Shank <jessishank@level11.com>
2018-05-21 14:20:53 -0700

    added 0.4.2

--- ocelot.rb
+++ ocelot.rb
@@ -1,9 +1,9 @@
 class Ocelot < Formula
   desc "the command line tool for ocelot CI"
   homepage "https://bitbucket.org/level11consulting/ocelot"
-  url "https://s3-us-west-2.amazonaws.com/ocelotty/mac-ocelot-0.4.1.zip"
+  url "https://s3-us-west-2.amazonaws.com/ocelotty/mac-ocelot-0.4.2.zip"
   sha256 "f839ae90ab3caaf4aae47e5255254c3b53c2ed58bcf2d4acf11ddaf37302469d"
-  version "0.4.1"
+  version "0.4.2"
 
   bottle :unneeded
 
