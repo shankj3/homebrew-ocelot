8d073e2f3a9b68bd73b746376719b1b05fbd4b5b
Jessi Shank <jessishank1@gmail.com>
2018-05-24 14:55:45 -0700

    aaactually, needed to bump the version

--- ocelot.rb
+++ ocelot.rb
@@ -2,8 +2,8 @@ class Ocelot < Formula
   desc "the command line tool for ocelot CI"
   homepage "https://bitbucket.org/level11consulting/ocelot"
   url "https://s3-us-west-2.amazonaws.com/ocelotty/mac-ocelot-0.4.2.zip"
-  sha256 "a8de3b5ef10e6f3dc5694be53236f12728dd79681be9e4e0a331f0c84090780f"
-  version "0.4.3a"
+  sha256 "104a8b5c92ba30a3adae035a32b82d001447b9385053693fe416ca8815fbcbbd"
+  version "0.5.0"
 
   bottle :unneeded
 
