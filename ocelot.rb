5580388cf4ba3479ee7e4364e67fb595efd61432
Jessi Shank <jessishank1@gmail.com>
2018-07-30 14:47:51 -0700

    updating sha

--- ocelot.rb
+++ ocelot.rb
@@ -2,7 +2,7 @@ class Ocelot < Formula
   desc "the command line tool for ocelot CI"
   homepage "https://bitbucket.org/level11consulting/ocelot"
   url "https://s3-us-west-2.amazonaws.com/ocelotty/mac-ocelot-0.6.0-61-ge54956c.zip"
-  sha256 "c688143b4acb55518a88be269afdf833a6a1e231609e1df3fb6fdad02ce70135"
+  sha256 "a24f2f7a0cdfdef6e7461210e71ad9d648753d3ed2808f05368611c1a34c955d"
   version "0.6.0-61-ge54956c"
 
   bottle :unneeded
