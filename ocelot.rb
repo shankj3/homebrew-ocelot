d024ca0370c366ff7e476611ceb38e8da6a40252
Jessi Shank <jessishank1@gmail.com>
2018-07-30 14:46:37 -0700

    updating to 0.6.0-61-ge54956c

--- ocelot.rb
+++ ocelot.rb
@@ -1,9 +1,9 @@
 class Ocelot < Formula
   desc "the command line tool for ocelot CI"
   homepage "https://bitbucket.org/level11consulting/ocelot"
-  url "https://s3-us-west-2.amazonaws.com/ocelotty/darwin-ocelot-0.6.0-45-g02dd812.zip"
-  sha256 "21166ba3a5192608fee9f8d81990c8552bf76a1ae1c977534ea21b815dac3f38"
-  version "0.6.0-45-g02dd812"
+  url "https://s3-us-west-2.amazonaws.com/ocelotty/mac-ocelot-0.6.0-61-ge54956c.zip"
+  sha256 "c688143b4acb55518a88be269afdf833a6a1e231609e1df3fb6fdad02ce70135"
+  version "0.6.0-61-ge54956c"
 
   bottle :unneeded
 
