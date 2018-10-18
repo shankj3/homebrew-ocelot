7492484f6f902ee1e90cde2066509f4cf619e027
Jessi Shank <jessishank1@gmail.com>
2018-10-18 08:31:44 -0700

    sha'd the wrong thing woopsi

--- ocelot.rb
+++ ocelot.rb
@@ -2,7 +2,7 @@ class Ocelot < Formula
   desc "the command line tool for ocelot CI"
   homepage "https://bitbucket.org/level11consulting/ocelot"
   url "https://github.com/shankj3/ocelot/releases/download/v0.7.3/darwin-ocelot-v0.7.3.zip"
-  sha256 "d9d843ab0e1c77d4d8710af1f323fdc7b76049f47422b3fd3fda8cf465d8a07e"
+  sha256 "fe2771fcb23bd23bf9afffde24df87100d598d5d83d548b935294edea2ea8222"
   version "v0.7.3"
 
   bottle :unneeded
